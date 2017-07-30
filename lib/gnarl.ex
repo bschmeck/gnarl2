012387d535cf8546e7f4dc40e3efa836064c6514
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-07-29 19:53:38 -0500

    Initial commit

---
+++ lib/gnarl.ex
@@ -0,0 +1,18 @@
+defmodule Gnarl do
+  @moduledoc """
+  Documentation for Gnarl.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gnarl.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
