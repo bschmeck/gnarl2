d45fb356bf7b00101105024a2a30834a0bf13a9b
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-08-21 09:59:48 -0500

    Consolidate common code in do_outcomes

--- lib/probability.ex
+++ lib/probability.ex
@@ -2,25 +2,25 @@ defmodule Probability do
   def outcomes(games), do: do_outcomes(games, [%Outcome{}])
 
   defp do_outcomes([], results), do: results
-  defp do_outcomes([game = %Game{away_prob: 0} | rest], results) do
-    rest
-    |> do_outcomes(results)
-    |> Enum.map(fn(o) -> add_winner(o, game.home_team, game.home_prob) end)
-  end
-  defp do_outcomes([game = %Game{home_prob: 0} | rest], results) do
-    rest
-    |> do_outcomes(results)
-    |> Enum.map(fn(o) -> add_winner(o, game.away_team, game.away_prob) end)
-  end
   defp do_outcomes([game | rest], results) do
     rest
     |> do_outcomes(results)
-    |> Enum.flat_map(fn(o) ->
+    |> Enum.flat_map(possible_winners_fn(game))
+  end
+
+  defp possible_winners_fn(%Game{away_prob: 0, home_team: team, home_prob: prob}) do
+    &([add_winner(&1, team, prob)])
+  end
+  defp possible_winners_fn(%Game{home_prob: 0, away_team: team, away_prob: prob}) do
+    &([add_winner(&1, team, prob)])
+  end
+  defp possible_winners_fn(game) do
+    fn(o) ->
       [
         add_winner(o, game.away_team, game.away_prob),
         add_winner(o, game.home_team, game.home_prob)
       ]
-    end)
+    end
   end
 
   defp add_winner(%Outcome{winners: winners, probability: probability}, winner, winner_probability) do
