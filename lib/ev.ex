b4d419dcea368d5c110a7ad845c054cfbbdeb87a
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-09-07 17:49:53 -0500

    Fix 2 bugs in the EV module:
    * We were double counting the score for a given outcome
    * We were incorrectly computing the probability of a given score.

--- lib/ev.ex
+++ lib/ev.ex
@@ -1,6 +1,9 @@
 defmodule EV do
   defstruct distribution: %{}, ev: 0
 
+  # Expect that picks is a list of all of the teams a player wants to win.  That is,
+  # picks is comprised of the teams the player picked to win and the teams the player's
+  # opponent picked to lose.
   def of(picks, outcomes) do
     dist = distribution_of(picks, outcomes)
     ev = ev_of(dist)
@@ -15,14 +18,23 @@ defmodule EV do
     #  distribution[score] += 1
     score = score_of(picks, outcome)
     dist = distribution_of(picks, rest)
-    prev = Map.get(dist, score) || 1
-    Map.put(dist, score, prev * outcome.probability)
+    prev = Map.get(dist, score) || 0
+    Map.put(dist, score, prev + outcome.probability)
   end
 
+  # The map step winds up double counting the score, because we're counting the difference
+  # in games picked correctly and games picked incorrectly, but are only concerned with the
+  # difference in games picked correctly.  Since they're inverses of each other, we wind up
+  # double counting and divide by 2 on the final step, instead of adding 0.5s and dealing
+  # with floats.
+  #
+  # E.g. Player A goes 5-3 and Player B goes 4-4, the map step will give 9 +1s and 7 -1s,
+  # for a total of +2, but the player only won by a single game.
   defp score_of(games, %Outcome{winners: winners}) do
     games
     |> Enum.map(fn(g) -> if Enum.member?(winners, g), do: 1, else: -1 end)
     |> Enum.reduce(fn(val, score) -> val + score end)
+    |> Kernel.div(2)
   end
 
   defp ev_of(distribution) do
--- test/ev_test.exs
+++ test/ev_test.exs
@@ -1,22 +1,54 @@
 defmodule EVTest do
   use ExUnit.Case
 
-  test "it computes EV of a single outcome" do
-    outcomes = [%Outcome{winners: ["A0"], probability: 1}]
-    assert EV.of(["A0"], outcomes) == %EV{ distribution: %{ 1 => 1 }, ev: 1 }
-    assert EV.of(["B0"], outcomes) == %EV{ distribution: %{ -1 => 1 }, ev: -1 }
-  end
-
   test "it computes EV when not all games are picked" do
-    outcomes = [%Outcome{winners: ["A0", "A1"], probability: 1}]
-    assert EV.of(["A0"], outcomes) == %EV{ distribution: %{ 1 => 1 }, ev: 1 }
-    assert EV.of(["B0"], outcomes) == %EV{ distribution: %{ -1 => 1 }, ev: -1 }
+    outcomes = [%Outcome{winners: ["A0", "A1", "A2"], probability: 0.125}, # score 0
+                %Outcome{winners: ["A0", "A1", "B2"], probability: 0.125}, # score 0
+                %Outcome{winners: ["A0", "B1", "A2"], probability: 0.125}, # score 1
+                %Outcome{winners: ["A0", "B1", "B2"], probability: 0.125}, # score 1
+                %Outcome{winners: ["B0", "A1", "A2"], probability: 0.125}, # score -1
+                %Outcome{winners: ["B0", "A1", "B2"], probability: 0.125}, # score -1
+                %Outcome{winners: ["B0", "B1", "A2"], probability: 0.125}, # score 0
+                %Outcome{winners: ["B0", "B1", "B2"], probability: 0.125}] # score 0
+    ev = EV.of(["A0", "B1"], outcomes)
+
+    assert ev.distribution == %{1 => 0.25, -1 => 0.25, 0 => 0.5}
+    assert ev.ev == 0
   end
 
-  test "it computes EV when not all games are decided" do
-    outcomes = [%Outcome{winners: ["A0"], probability: 0.1},
-                %Outcome{winners: ["B0"], probability: 0.9}]
-    assert EV.of(["A0"], outcomes) == %EV{ distribution: %{ 1 => 0.1, -1 => 0.9 }, ev: -0.8 }
-    assert EV.of(["B0"], outcomes) == %EV{ distribution: %{ 1 => 0.9, -1 => 0.1 }, ev: 0.8 }
+  test "it computes EV of multiple games" do
+    outcomes = [%Outcome{winners: ["A0", "A1"], probability: 0.1}, # score 0
+                %Outcome{winners: ["A0", "B1"], probability: 0.4}, # score 1
+                %Outcome{winners: ["B0", "A1"], probability: 0.2}, # score -1
+                %Outcome{winners: ["B0", "B1"], probability: 0.3}] # score 0
+    ev = EV.of(["A0", "B1"], outcomes)
+
+    assert ev.distribution == %{1 => 0.4, -1 => 0.2, 0 => 0.4}
+    assert ev.ev == 0.2
+  end
+
+  test "distribution values sum to 1" do
+    outcomes = [
+      %Game{home_team: "A0", home_prob: 0.75, away_team: "B0", away_prob: 0.25},
+      %Game{home_team: "A1", home_prob: 0.75, away_team: "B1", away_prob: 0.25},
+      %Game{home_team: "A2", home_prob: 0.75, away_team: "B2", away_prob: 0.25},
+      %Game{home_team: "A3", home_prob: 0.75, away_team: "B3", away_prob: 0.25},
+      %Game{home_team: "A4", home_prob: 0.75, away_team: "B4", away_prob: 0.25},
+      %Game{home_team: "A5", home_prob: 0.75, away_team: "B5", away_prob: 0.25},
+      %Game{home_team: "A6", home_prob: 0.75, away_team: "B6", away_prob: 0.25},
+      %Game{home_team: "A7", home_prob: 0.75, away_team: "B7", away_prob: 0.25},
+      %Game{home_team: "A8", home_prob: 0.75, away_team: "B8", away_prob: 0.25},
+      %Game{home_team: "A9", home_prob: 0.75, away_team: "B9", away_prob: 0.25},
+      %Game{home_team: "AA", home_prob: 0.75, away_team: "BA", away_prob: 0.25},
+      %Game{home_team: "AB", home_prob: 0.75, away_team: "BB", away_prob: 0.25},
+      %Game{home_team: "AC", home_prob: 0.75, away_team: "BC", away_prob: 0.25},
+      %Game{home_team: "AD", home_prob: 0.75, away_team: "BD", away_prob: 0.25},
+      %Game{home_team: "AE", home_prob: 0.75, away_team: "BE", away_prob: 0.25},
+      %Game{home_team: "AF", home_prob: 0.75, away_team: "BF", away_prob: 0.25},
+    ] |> Probability.outcomes
+
+    %EV{distribution: dist} = EV.of(~w(A0 B1 A2 A3 B4 B5 B5 A6 B7 A8 A9 AA BB BC BD BE AF), outcomes)
+
+    assert dist |> Map.values |> Enum.reduce(fn(a, b) -> a + b end) == 1
   end
 end
