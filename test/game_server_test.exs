4dab505abddf5550d557512f4fb590a2d1c08439
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-09-11 22:43:19 -0500

    Use the GenServer API to fetch games from the state when testing

--- test/game_server_test.exs
+++ test/game_server_test.exs
@@ -2,45 +2,59 @@ defmodule GameServerTest do
   use ExUnit.Case
 
   test "it adds new games" do
+    state = %{}
     score = %Game{away_team: "A0", away_score: 0, home_team: "B0", home_score: 0, time_left: "PREGAME"}
-    {:noreply, games} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, %{})
+    {:noreply, state} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, state)
+
+    {:reply, {:ok, games}, _state} = GameServer.handle_call({:games, 2017, 1}, nil, state)
+
     assert games |> Map.values |> Enum.count == 1
   end
 
   test "it updates existing games" do
+    state = %{}
     score = %Game{away_team: "A0", away_score: 0, home_team: "B0", home_score: 0, time_left: "PREGAME"}
-    {:noreply, games} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, %{})
+    {:noreply, state} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, state)
     score = %Game{away_team: "A0", away_score: 10, home_team: "B0", home_score: 21, time_left: "HALFTIME"}
-    {:noreply, games} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, games)
+    {:noreply, state} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, state)
 
-    assert games |> Map.get("2017-1") |> Map.values |> hd == score
+    {:reply, {:ok, games}, _state} = GameServer.handle_call({:games, 2017, 1}, nil, state)
+
+    assert games |> Map.values |> hd == score
   end
 
   test "it adds probabilities to existing games when given the away team's prob" do
+    state = %{}
     score = %Game{away_team: "A0", away_score: 0, home_team: "B0", home_score: 0, time_left: "PREGAME"}
-    {:noreply, games} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, %{})
-    {:noreply, games} = GameServer.handle_cast({:update_probabilities, 2017, 1, [{"A0", 0.75}]}, games)
+    {:noreply, state} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, state)
+    {:noreply, state} = GameServer.handle_cast({:update_probabilities, 2017, 1, [{"A0", 0.75}]}, state)
 
-    game = games |> Map.get("2017-1") |> Map.values |> hd
+    {:reply, {:ok, games}, _state} = GameServer.handle_call({:games, 2017, 1}, nil, state)
+
+    game = games |> Map.values |> hd
     assert game.away_prob == 0.75
     assert game.home_prob == 0.25
   end
 
   test "it adds probabilities to existing games when given the home team's prob" do
+    state = %{}
     score = %Game{away_team: "A0", away_score: 0, home_team: "B0", home_score: 0, time_left: "PREGAME"}
-    {:noreply, games} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, %{})
-    {:noreply, games} = GameServer.handle_cast({:update_probabilities, 2017, 1, [{"B0", 0.82}]}, games)
+    {:noreply, state} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, state)
+    {:noreply, state} = GameServer.handle_cast({:update_probabilities, 2017, 1, [{"B0", 0.82}]}, state)
 
-    game = games |> Map.get("2017-1") |> Map.values |> hd
+    {:reply, {:ok, games}, _state} = GameServer.handle_call({:games, 2017, 1}, nil, state)
+
+    game = games |> Map.values |> hd
     assert game.away_prob == 0.18
     assert game.home_prob == 0.82
   end
 
   test "it ignores probabilities for non-existent games" do
+    state = %{}
     score = %Game{away_team: "A0", away_score: 0, home_team: "B0", home_score: 0, time_left: "PREGAME"}
-    {:noreply, old_games} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, %{})
-    {:noreply, new_games} = GameServer.handle_cast({:update_probabilities, 2017, 1, [{"B1", 0.82}]}, old_games)
+    {:noreply, old_state} = GameServer.handle_cast({:update_scores, 2017, 1, [score]}, state)
+    {:noreply, new_state} = GameServer.handle_cast({:update_probabilities, 2017, 1, [{"B1", 0.82}]}, old_state)
 
-    assert old_games == new_games
+    assert old_state == new_state
   end
 end
