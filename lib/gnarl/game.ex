6a5198c8a92cfade37d2009e4b50aab15c0bc351
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-09-23 16:17:51 -0500

    Add changeset to Game module

--- lib/gnarl/game.ex
+++ lib/gnarl/game.ex
@@ -1,10 +1,18 @@
 defmodule Gnarl.Game do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "games" do
     belongs_to :home_team, Gnarl.Team
     belongs_to :away_team, Gnarl.Team
     belongs_to :week, Gnarl.Week
     field :winning_team, :string
+  end
+
+  def changeset(game, params \\ %{}) do
+    game
+    |> cast(params, [:home_team_id, :away_team_id, :week_id, :winning_team])
+    |> validate_required([:home_team_id, :away_team_id, :week_id])
+    |> validate_inclusion(:winning_team, [nil, "HOME", "AWAY"])
   end
 end
