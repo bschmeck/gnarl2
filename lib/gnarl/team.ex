ec0bd97186a66932878ecf36955165bffcf57259
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-10-16 21:19:45 -0500

    Create a changeset for a Team

--- lib/gnarl/team.ex
+++ lib/gnarl/team.ex
@@ -1,5 +1,6 @@
 defmodule Gnarl.Team do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "teams" do
     field :name, :string
@@ -7,5 +8,12 @@ defmodule Gnarl.Team do
     belongs_to :franchise, Gnarl.Franchise
     has_many :home_games, Gnarl.Game, foreign_key: :home_team_id
     has_many :away_games, Gnarl.Game, foreign_key: :away_team_id
+    belongs_to :season, Gnarl.Season
+  end
+
+  def changeset(team, params \\ %{}) do
+    team
+    |> cast(params, [:name, :abbreviation, :franchise_id, :season_id])
+    |> validate_required([:name, :abbreviation, :franchise_id, :season_id])
   end
 end
