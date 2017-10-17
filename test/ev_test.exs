a345bb837b7b240d67dfd3cd842d599a07d78146
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2017-10-16 22:38:27 -0500

    Use property tests to ensure distribution generated by Ev.of always sums to 1

--- test/ev_test.exs
+++ test/ev_test.exs
@@ -1,5 +1,6 @@
 defmodule EVTest do
   use ExUnit.Case, async: true
+  use Quixir
 
   test "it computes EV when not all games are picked" do
     outcomes = [%Outcome{winners: ["A0", "A1", "A2"], probability: 0.125}, # score 0
@@ -28,28 +29,21 @@ defmodule EVTest do
   end
 
   test "distribution values sum to 1" do
-    outcomes = [
-      %Game{home_team: "A0", home_prob: 0.75, away_team: "B0", away_prob: 0.25},
-      %Game{home_team: "A1", home_prob: 0.75, away_team: "B1", away_prob: 0.25},
-      %Game{home_team: "A2", home_prob: 0.75, away_team: "B2", away_prob: 0.25},
-      %Game{home_team: "A3", home_prob: 0.75, away_team: "B3", away_prob: 0.25},
-      %Game{home_team: "A4", home_prob: 0.75, away_team: "B4", away_prob: 0.25},
-      %Game{home_team: "A5", home_prob: 0.75, away_team: "B5", away_prob: 0.25},
-      %Game{home_team: "A6", home_prob: 0.75, away_team: "B6", away_prob: 0.25},
-      %Game{home_team: "A7", home_prob: 0.75, away_team: "B7", away_prob: 0.25},
-      %Game{home_team: "A8", home_prob: 0.75, away_team: "B8", away_prob: 0.25},
-      %Game{home_team: "A9", home_prob: 0.75, away_team: "B9", away_prob: 0.25},
-      %Game{home_team: "AA", home_prob: 0.75, away_team: "BA", away_prob: 0.25},
-      %Game{home_team: "AB", home_prob: 0.75, away_team: "BB", away_prob: 0.25},
-      %Game{home_team: "AC", home_prob: 0.75, away_team: "BC", away_prob: 0.25},
-      %Game{home_team: "AD", home_prob: 0.75, away_team: "BD", away_prob: 0.25},
-      %Game{home_team: "AE", home_prob: 0.75, away_team: "BE", away_prob: 0.25},
-      %Game{home_team: "AF", home_prob: 0.75, away_team: "BF", away_prob: 0.25},
-    ] |> Probability.outcomes
+    ptest games: list(of: Pollution.VG.struct(%Game{home_team: string(min: 2, max: 3, chars: :upper),
+                                                home_prob: float(min: 0.0, max: 1.0),
+                                                home_score: positive_int(),
+                                                away_team: string(min: 2, max: 3, chars: :upper),
+                                                away_score: positive_int(),
+                                                time_left: string(chars: :printable, max: 5)
+                                               }), min: 1, max: 16) do
+      outcomes = games
+      |> Enum.map(fn(game) -> %Game{game | away_prob: 1 - game.home_prob} end)
+      |> Probability.outcomes
 
-    %EV{distribution: dist} = EV.of(~w(A0 B1 A2 A3 B4 B5 B5 A6 B7 A8 A9 AA BB BC BD BE AF), outcomes)
+      %EV{distribution: dist} = outcomes |> Enum.take_random(1) |> EV.of(outcomes)
 
-    assert dist |> Map.values |> Enum.reduce(fn(a, b) -> a + b end) == 1
+      assert_in_delta dist |> Map.values |> Enum.reduce(fn(a, b) -> a + b end), 1.0, 0.0000001
+    end
   end
 
   test "when a loss is not possible" do
