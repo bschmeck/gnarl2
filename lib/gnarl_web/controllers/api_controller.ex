ea03523d436f312b0181efdc0b3563ca5f7a1990
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2017-11-10 08:38:14 -0600

    Return actual scores from the api/scores endpoint

--- lib/gnarl_web/controllers/api_controller.ex
+++ lib/gnarl_web/controllers/api_controller.ex
@@ -8,17 +8,9 @@ defmodule GnarlWeb.ApiController do
   end
 
   def scores(conn, _params) do
-    json conn, [%{"fields": %{"picker": "BEN", "home_team": "PHI", "away_team": "DEN", "picked_team": "PHI", "lock": false, "time_left": "Final", "home_score": 51, "anti_lock": false, "away_score": 23}},
-                %{"fields": %{"picker": "BEN", "home_team": "SEA", "away_team": "WAS", "picked_team": "SEA", "lock": false, "time_left": "Final", "home_score": 14, "anti_lock": false, "away_score": 17}},
-                %{"fields": %{"picker": "BEN", "home_team": "NYG", "away_team": "LAR", "picked_team": "LAR", "lock": false, "time_left": "Final", "home_score": 17, "anti_lock": true, "away_score": 51}},
-                %{"fields": %{"picker": "BEN", "home_team": "NYJ", "away_team": "BUF", "picked_team": "BUF", "lock": false, "time_left": "Final", "home_score": 34, "anti_lock": false, "away_score": 21}},
-                %{"fields": %{"picker": "BEN", "home_team": "DAL", "away_team": "KC", "picked_team": "KC", "lock": false, "time_left": "Final", "home_score": 28, "anti_lock": false, "away_score": 17}},
-                %{"fields": %{"picker": "BEN", "home_team": "GB", "away_team": "DET", "picked_team": "DET", "lock": true, "time_left": "Final", "home_score": 17, "anti_lock": false, "away_score": 30}},
-                %{"fields": %{"picker": "BRIAN", "home_team": "HOU", "away_team": "IND", "picked_team": "HOU", "lock": false, "time_left": "Final", "home_score": 14, "anti_lock": false, "away_score": 20}},
-                %{"fields": %{"picker": "BRIAN", "home_team": "NO", "away_team": "TB", "picked_team": "NO", "lock": false, "time_left": "Final", "home_score": 30, "anti_lock": true, "away_score": 10}},
-                %{"fields": %{"picker": "BRIAN", "home_team": "JAC", "away_team": "CIN", "picked_team": "JAC", "lock": false, "time_left": "Final", "home_score": 23, "anti_lock": false, "away_score": 7}},
-                %{"fields": %{"picker": "BRIAN", "home_team": "TEN", "away_team": "BAL", "picked_team": "TEN", "lock": false, "time_left": "Final", "home_score": 23, "anti_lock": false, "away_score": 20}},
-                %{"fields": %{"picker": "BRIAN", "home_team": "MIA", "away_team": "OAK", "picked_team": "OAK", "lock": true, "time_left": "Final", "home_score": 24, "anti_lock": false, "away_score": 27}},
-                %{"fields": %{"picker": "BRIAN", "home_team": "CAR", "away_team": "ATL", "picked_team": "CAR", "lock": false, "time_left": "Final", "home_score": 20, "anti_lock": false, "away_score": 17}}]
+    with {:ok, games} <- GameServer.games({2017, 10}) do
+      scores = games |> Enum.map(fn {key, game} -> %{fields: game |> Map.from_struct} end)
+      json conn, scores
+    end
   end
 end
