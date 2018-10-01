647c72b86a9293941a1ceb2956ee67530fd1f0d3
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2018-10-01 18:26:00 -0500

    Create Picks and send them to the PicksServer

--- lib/gnarl_web/controllers/admin/picks_controller.ex
+++ lib/gnarl_web/controllers/admin/picks_controller.ex
@@ -23,4 +23,20 @@ defmodule GnarlWeb.Admin.PicksController do
     |> assign(:week, week)
     |> render("show.html")
   end
+
+  def create(conn, params = %{"season" => season, "week" => week}) when is_binary(season) and is_binary(week) do
+    {season, ""} = Integer.parse(season)
+    {week, ""} = Integer.parse(week)
+
+    params = params |> Map.put("season", season) |> Map.put("week", week)
+    create(conn, params)
+  end
+
+  def create(conn, %{"season" => season, "week" => week, "ben_picks" => ben_picks, "brian_picks" => brian_picks, "ben_lock" => ben_lock, "brian_lock" => brian_lock, "ben_antilock" => ben_antilock, "brian_antilock" => brian_antilock}) do
+    ben_picks = ben_picks |> Enum.reject(&(&1 == ""))
+    brian_picks = brian_picks |> Enum.reject(&(&1 == ""))
+    picks = Gnarl.Pick.build("BEN", ben_picks, ben_lock, brian_antilock) ++ Gnarl.Pick.build("BRIAN", brian_picks, brian_lock, ben_antilock)
+    PicksServer.set_picks(season, week, picks)
+    redirect conn, to: page_path(conn, :index)
+  end
 end
