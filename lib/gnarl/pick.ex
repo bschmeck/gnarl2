efbec0e84d36d6fe764d2b775c2bf8abb0aebe4c
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2018-10-01 18:25:31 -0500

    Build Pick structs from a list of teams

--- lib/gnarl/pick.ex
+++ lib/gnarl/pick.ex
@@ -1,4 +1,13 @@
 defmodule Gnarl.Pick do
   @enforce_keys [:picker, :winner, :slot]
   defstruct picker: '', winner: '', slot: 0, lock: false, antilock: false
+
+  def build(picker, winners, lock, antilock), do: build(picker, winners, lock, antilock, 1)
+  defp build(_picker, [], _lock, _antilock, _slot), do: []
+  defp build(picker, [winner | rest], lock, antilock, slot) do
+    [
+      %__MODULE__{picker: picker, winner: winner, slot: slot, lock: winner == lock, antilock: winner == antilock} |
+      build(picker, rest, lock, antilock, slot + 1)
+    ]
+  end
 end
